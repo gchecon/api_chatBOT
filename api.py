b78be07bfac65391a474a8486df3151a9f6356c5
gchecon <42677354+gchecon@users.noreply.github.com>
2024-03-19 11:04:54 -0300

    Não está funcionando por erro na OpenAI. Testar em outro ambiente

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from openai import OpenAI
+from typing import List
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,19 +9,42 @@ client = OpenAI()
 
 app = FastAPI()
 
+
 class Query(BaseModel):
     text: str
     max_tokens: int = 50
     temperature: float = 0.5
 
+
+class Message(BaseModel):
+    role: str
+    content: str
+
+
+class ChatRequest(BaseModel):
+    model: str = "gpt-3.5-turbo"
+    messages: List[Message]
+
+
 @app.post("/query/")
 async def query_model(query: Query):
     try:
-        response = client.completions.create(model="gpt-3.5-turbo-16k",  # Ou o identificador atual para a GPT-4-turbo
-        prompt=query.text,
-        max_tokens=query.max_tokens,
-        temperature=query.temperature)
+        response = client.completions.create(model="gpt-3.5-turbo",  # Ou o identificador atual para a GPT-4-turbo
+                                             prompt=query.text,
+                                             max_tokens=query.max_tokens,
+                                             temperature=query.temperature)
         return {"response": response.choices[0].text.strip()}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+
+@app.post("/chat/")
+async def chat(chat_requests: ChatRequest):
+    try:
+        response = client.chat.completions.create(
+            model=chat_requests.model,
+            messages=[message.dict() for message in chat_requests.messages]
+        )
+        return response
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
